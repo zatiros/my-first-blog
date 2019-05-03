fd2fb5141c8a9906183e28b5e13705a71236a493
zatiros <daniel-gama92@hotmail.com>
2019-05-03 17:32:14 -0300

    Primeira View do Blog criada.

--- bl/settings.py
+++ bl/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8p*=a0^qy!fe**#7(g*fp@#l99ab6+bm%^hvo+5o@de6c3d#wt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.pythonanywhere.com']
 
 
 # Application definition

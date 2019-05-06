c211f00c704d5c7f086319bdf15deb0103c3993d
zatiros <daniel-gama92@hotmail.com>
2019-05-06 12:52:03 -0300

    Modificacao de templates e posts para o banco de dados

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
-# Create your models here.
+
+
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
